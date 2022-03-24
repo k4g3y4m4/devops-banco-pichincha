8a0837971c107c7fd57c6f4572194a5251637499
amberdevpe <amberdevpe@gmail.com>
2022-03-24 09:01:07 -0500

    add terraform

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app-server" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Devsu-devops"
+  }
+}
